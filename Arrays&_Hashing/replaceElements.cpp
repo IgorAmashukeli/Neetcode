368b79fd2c1b31383fd233976834fe7f363045f6
IgorAmashukeli <amashukeliim1150@gmail.com>
2023-08-21 16:34:51 +0300

    solving

---
+++ Arrays&_Hashing/replaceElements.cpp
@@ -0,0 +1,61 @@
+/**Given an array arr, replace every element in that array with the greatest
+element among the elements to its right, and replace the last element with -1.
+
+After doing so, return the array.
+
+
+
+Example 1:
+
+Input: arr = [17,18,5,4,6,1]
+Output: [18,6,6,6,1,-1]
+Explanation:
+- index 0 --> the greatest element to the right of index 0 is index 1 (18).
+- index 1 --> the greatest element to the right of index 1 is index 4 (6).
+- index 2 --> the greatest element to the right of index 2 is index 4 (6).
+- index 3 --> the greatest element to the right of index 3 is index 4 (6).
+- index 4 --> the greatest element to the right of index 4 is index 5 (1).
+- index 5 --> there are no elements to the right of index 5, so we put -1.
+Example 2:
+
+Input: arr = [400]
+Output: [-1]
+Explanation: There are no elements to the right of index 0.
+
+
+Constraints:
+
+1 <= arr.length <= 10^4
+1 <= arr[i] <= 10^5
+
+idea: find current maximum from end:
+if s is the maximum value from j + 1 position to the end
+then output[j] = s
+**/
+
+#include <vector>
+
+using namespace std;
+
+vector<int> replaceElements(vector<int> &arr) {
+  // check for empty array
+  if (arr.size() == 0) {
+    return arr;
+  }
+
+  int max_right = -1;
+  for (size_t i = 0; i < arr.size(); ++i) {
+    size_t j = arr.size() - i - 1;
+
+    // save arr[j] to calculate maximum value after
+    int arr_j = arr[j];
+
+    // replace arr[j] with maximum value found so far
+    arr[j] = max_right;
+
+    // update maximum value according to the new proccessed element
+    max_right = max(arr_j, max_right);
+  }
+
+  return arr;
+}
