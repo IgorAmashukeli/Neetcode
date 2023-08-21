324630ca951b9af8c7eb18d083dc113ccad19bd4
IgorAmashukeli <amashukeliim1150@gmail.com>
2023-08-22 02:22:21 +0300

    solving

---
+++ Arrays&_Hashing/10_numUniqueEmails.cpp
@@ -0,0 +1,84 @@
+/**Every valid email consists of a local name and a domain name, separated by
+the '@' sign. Besides lowercase letters, the email may contain one or more '.'
+or '+'.
+
+For example, in "alice@leetcode.com", "alice" is the local name, and
+"leetcode.com" is the domain name. If you add periods '.' between some
+characters in the local name part of an email address, mail sent there will be
+forwarded to the same address without dots in the local name. Note that this
+rule does not apply to domain names.
+
+For example, "alice.z@leetcode.com" and "alicez@leetcode.com" forward to the
+same email address. If you add a plus '+' in the local name, everything after
+the first plus sign will be ignored. This allows certain emails to be filtered.
+Note that this rule does not apply to domain names.
+
+For example, "m.y+name@email.com" will be forwarded to "my@email.com".
+It is possible to use both of these rules at the same time.
+
+Given an array of strings emails where we send one email to each emails[i],
+return the number of different addresses that actually receive mails.
+
+
+
+Example 1:
+
+Input: emails =
+["test.email+alex@leetcode.com","test.e.mail+bob.cathy@leetcode.com","testemail+david@lee.tcode.com"]
+Output: 2
+Explanation: "testemail@leetcode.com" and "testemail@lee.tcode.com" actually
+receive mails. Example 2:
+
+Input: emails = ["a@leetcode.com","b@leetcode.com","c@leetcode.com"]
+Output: 3
+
+
+Constraints:
+
+1 <= emails.length <= 100
+1 <= emails[i].length <= 100
+emails[i] consist of lowercase English letters, '+', '.' and '@'.
+Each emails[i] contains exactly one '@' character.
+All local and domain names are non-empty.
+Local names do not start with a '+' character.
+Domain names end with the ".com" suffix.
+
+idea: straigtforward conversion of strings + unordered_set to count numbers
+**/
+
+#include <algorithm>
+#include <string>
+#include <unordered_set>
+#include <vector>
+
+using namespace std;
+
+string convert(const std::string &email) {
+  // find local name
+  std::string local_name = email.substr(0, email.find('@'));
+
+  // find domain name
+  std::string domain_name = email.substr(email.find('@'), email.size());
+
+  // if there is '+', delete everything after first '+'
+  if (local_name.find('+') != local_name.npos) {
+    local_name = local_name.substr(0, local_name.find_first_of('+'));
+  }
+
+  // erase + remove all '.' in local_name
+  local_name.erase(remove(local_name.begin(), local_name.end(), '.'),
+                   local_name.end());
+
+  // restore email name
+  std::string name = local_name + '@' + domain_name;
+
+  return name;
+}
+
+int numUniqueEmails(vector<string> &emails) {
+  unordered_set<string> unique_emails;
+  for (size_t i = 0; i < emails.size(); ++i) {
+    unique_emails.insert(convert(emails[i]));
+  }
+  return unique_emails.size();
+}
